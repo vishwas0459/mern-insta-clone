a5a61ae5cf788b0fd705903512d3ff5eaa24e002
Vishwas Meshram <vishwasmeshram0459@gmail.com>
2019-08-03 00:33:15 +0530

    initial login component

---
+++ client/src/components/login/login.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    data: null
+  };
+  onSubmitLogin = async event => {
+    event.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    console.log('Login Form Submitted!!!', user);
+    const resp = await axios.post('/login', user);
+    this.setState({ data: resp });
+    //TODO:: Get the token from backend after successful login and redirect to new page
+    console.log(this.state.data);
+  };
+  handleInputChange = ({ currentTarget }) => {
+    // console.log(currentTarget.value);
+    this.setState({ [currentTarget.id]: currentTarget.value });
+  };
+  render() {
+    return (
+      <div className="container">
+        {this.state.data && JSON.stringify(this.state.data.data)}
+        <form onSubmit={this.onSubmitLogin}>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              value={this.state.email}
+              onChange={event => this.handleInputChange(event)}
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={event => this.handleInputChange(event)}
+            />
+          </div>
+          <div className="form-group form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
