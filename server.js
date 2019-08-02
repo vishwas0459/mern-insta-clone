2f09c05fd3867140a61c222056311a95b96af2db
Vishwas Meshram <vishwasmeshram0459@gmail.com>
2019-08-03 00:50:37 +0530

    added login route

--- server.js
+++ server.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+app.use(express.json());
 
+mongoose
+  .connect('mongodb://localhost:27017/mern-insta-clone-db')
+  .then(db => {
+    console.log('Connected to db...');
+    // createUser();
+  })
+  .catch(err => console.log('Something went wrong'));
+
+function createUser() {
+  const userSchema = mongoose.Schema({
+    email: String,
+    password: String
+  });
+
+  const User = mongoose.model('User', userSchema);
+  const newUser = new User({
+    email: 'vishwas',
+    password: 'meshram'
+  });
+  newUser.save();
+}
+app.get('/', (req, res) => {
+  res.send('Hello from express server!!!');
+});
+
+app.post('/api/login', (req, res) => {
+  console.log('client data', req.body);
+});
+
+//TODO: setup login with mLab
 const port = 3002;
 app.listen(port, () =>
   console.log(`Express server is running on port: ${port}`)
